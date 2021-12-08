46310ab87892d2c305786dc8be01e4e214297255
Antônio Pedro <antoniopedro.rs9@gmail.com>
2021-12-08 14:58:22 -0300

    Criação do .gitignore e mudança na indentação

--- bot.js
+++ bot.js
@@ -4,99 +4,84 @@ const POST_URL = "";
 
 let profilesToMention = [];
 
-
-
 const puppeteer = require("puppeteer");
 
 let browser, page, comments = 0;
 
 (async function main() {
+  browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized", "--no--sandbox", "--disable--setuid--sandbox"],
+  });
 
-	browser = await puppeteer.launch({
-		headless: false,
-		defaultViewport: null,
-		args: ["--start-maximized", "--no--sandbox", "--disable--setuid--sandbox"]
-	});
+  page = (await browser.pages())[0];
 
-	page = (await browser.pages())[0];
-
-	await login();
-	await comment();
+  await login();
+  await comment();
 })();
 
-
-
 async function login() {
-	await page.goto("https://www.instagram.com/", { waitUntil: "domcontentloaded" });
+  await page.goto("https://www.instagram.com/", {
+    waitUntil: "domcontentloaded",
+  });
 
-	console.log(`\nConnecting...`);
+  console.log(`\nConnecting...`);
 
-	await typeIn("input[name='username']", USERNAME);
-	await typeIn("input[name='password']", PASSWORD);
-	await clickAt("button[type='submit']");
-	await page.waitForSelector("span[class='TqC_a']");
+  await typeIn("input[name='username']", USERNAME);
+  await typeIn("input[name='password']", PASSWORD);
+  await clickAt("button[type='submit']");
+  await page.waitForSelector("span[class='TqC_a']");
 
-	console.log("\nLogged in!");
+  console.log("\nLogged in!");
 }
-
-
 
 async function comment() {
-	await page.goto(POST_URL, { waitUntil: "domcontentloaded" });
+  await page.goto(POST_URL, { waitUntil: "domcontentloaded" });
 
-	console.log(`\nBot started!`);
+  console.log(`\nBot started!`);
 
-	while (true) {
-		let text = await pickProfiles();
+  while (true) {
+    let text = await pickProfiles();
 
-		await typeIn("textarea[class='Ypffh']", text);
-		await clickAt("button[type='submit']");
+    await typeIn("textarea[class='Ypffh']", text);
+    await clickAt("button[type='submit']");
 
-		comments++
-		console.log(`\nComments: ${comments}\n${text}`);
+    comments++;
+    console.log(`\nComments: ${comments}\n${text}`);
 
-		let randomInterval = 40 + Math.round(Math.random() * 40);
+    let randomInterval = 40 + Math.round(Math.random() * 40);
 
-		await sleep(randomInterval);
-	}
+    await sleep(randomInterval);
+  }
 }
-
-
 
 async function pickProfiles() {
-	while (true) {
-		let randomIndex1 = Math.floor(Math.random() * profilesToMention.length);
-		let randomIndex2 = Math.floor(Math.random() * profilesToMention.length);
+  while (true) {
+    let randomIndex1 = Math.floor(Math.random() * profilesToMention.length);
+    let randomIndex2 = Math.floor(Math.random() * profilesToMention.length);
 
-		let profile1 = profilesToMention[randomIndex1];
-		let profile2 = profilesToMention[randomIndex2];
-		
-		if (profile1 !== profile2) {
-			return `@${profile1} @${profile2}`;
-		}
-	}
+    let profile1 = profilesToMention[randomIndex1];
+    let profile2 = profilesToMention[randomIndex2];
+
+    if (profile1 !== profile2) {
+      return `@${profile1} @${profile2}`;
+    }
+  }
 }
-
-
 
 async function clickAt(element) {
-	await page.waitForSelector(`${element}`);
-	await page.click(`${element}`);
+  await page.waitForSelector(`${element}`);
+  await page.click(`${element}`);
 }
-
-
 
 async function typeIn(element, text) {
-	await page.waitForSelector(`${element}`);
-	await page.type(`${element}`, text, { delay: 100 });
+  await page.waitForSelector(`${element}`);
+  await page.type(`${element}`, text, { delay: 100 });
 }
 
-
-
 async function sleep(seconds) {
-	milliseconds = seconds * 1000;
-
-	return new Promise((resolve) => {
-		setTimeout(resolve, milliseconds);
-	});
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds * 1000);
+  });
 }
