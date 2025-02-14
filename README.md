58ba2b493c56f76d646899f3403e633ced0b9337
AntonioPedro9 <antoniopedro.rs9@gmail.com>
2025-02-14 18:21:50 -0300

    Melhoria na legibilidade do código

--- README.md
+++ README.md
@@ -6,19 +6,19 @@ The bot can perform approximately 1.400 tags within 24 hours.
 
 ## How to use
 
-In the `bot.js` file, fill in the following information:
+In `bot.js` file, fill in the following information:
 
 ```
 const USERNAME = ""; // Instagram username
 const PASSWORD = ""; // Instagram password
 const POST_URL = ""; // URL of the giveaway post
-const MENTIONS = 2;  // Number of people to tag per comment
+const NUMBER_OF_PROFILES_TO_MENTION MENTIONS = 2;
 ```
 
-Next, add the usernames of the people you want to tag in the post, for example:
+Next, ddd the username of the profiles you want to tag in the post, for example:
 
 ```
-let profilesToMention = ["@user1", "@user2", "@user3", "@user4"];
+let profilesList = ["@user1", "@user2", "@user3", "@user4"];
 ```
 
 ## Running the Application
--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const USERNAME = "";
 const PASSWORD = "";
 const POST_URL = "";
-const MENTIONS = 2;
-
-let profilesToMention = ["@usuario1", "@usuario2", "@usuario3", "@usuario4"];
+const NUMBER_OF_PROFILES_TO_MENTIONS = 2;
 
 const puppeteer = require("puppeteer");
 
+const profilesList = ["@usuario1", "@usuario2", "@usuario3", "@usuario4"];
+
 let browser,
   page,
-  comments = 0;
+  commentCount = 0;
 
 (async function main() {
   browser = await puppeteer.launch({
@@ -56,15 +56,15 @@ async function comment() {
       return;
     }
 
-    let comment = await pickProfiles();
+    let comment = await selectProfilesToMention();
 
     await typeIn("textarea[autocomplete='off'][autocorrect='off']", comment);
     await clickAt(
       "div[class='x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x1lku1pv x1a2a7pz x6s0dn4 xjyslct x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 x1ypdohk x1i0vuye xwhw2v2 xl56j7k x17ydfre x1f6kntn x2b8uid xlyipyv x87ps6o x14atkfc x1d5wrs8 x972fbf xcfux6l x1qhh985 xm0m39n xm3z3ea x1x8b98j x131883w x16mih1h xt0psk2 xt7dq6l xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 xjbqb8w x1n5bzlp x173jzuc x1yc6y37']"
     );
 
-    comments++;
-    console.log(`\nComments: ${comments}\n${comment}`);
+    commentCount++;
+    console.log(`\nComment count: ${commentCount}\n${comment}`);
 
     let randomInterval = 40 + Math.round(Math.random() * 40);
 
@@ -72,14 +72,14 @@ async function comment() {
   }
 }
 
-async function pickProfiles() {
+async function selectProfilesToMention() {
   var comment = "";
 
   let i = 0;
 
-  while (i < MENTIONS) {
-    let randomIndex = Math.floor(Math.random() * profilesToMention.length);
-    let currentProfile = profilesToMention[randomIndex];
+  while (i < NUMBER_OF_PROFILES_TO_MENTIONS) {
+    let randomIndex = Math.floor(Math.random() * profilesList.length);
+    let currentProfile = profilesList[randomIndex];
 
     if (!comment.includes(currentProfile)) {
       comment += currentProfile + " ";
